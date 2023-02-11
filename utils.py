83cb51c92b3613ba44d46fad6d4e8827cc01b329
lohith <lohith.etl4343@gmail.com>
2023-02-11 09:59:27 -0500

    First commit

---
+++ utils.py
@@ -0,0 +1,24 @@
+import os
+import datetime
+import shutil
+
+# creating a audio logs folder where all the audio files are written to
+def create_log_dir(dir=None):
+    if dir is None:
+        current_working_dir = os.getcwd()
+        audio_folder = os.path.join(current_working_dir, "audio_logs")
+    else:
+        if os.path.isdir(dir):
+            audio_folder = os.path.join(dir.rstrip('//'), "audio_logs")
+
+    if os.path.exists(audio_folder):
+        shutil.rmtree(audio_folder)
+        os.makedirs(audio_folder)
+    else:
+        os.makedirs(audio_folder)
+        print(f"{audio_folder} has been created.")
+    
+    return audio_folder
+
+def get_time_stamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
